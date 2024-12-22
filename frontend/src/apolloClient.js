4a26ab8b148ff42b8d127f594ff34604344c89b0
Baibhab <baibhabnayak.31@gmail.com>
2024-12-22 15:47:19 +0530

    Fix API endpoint URI in Apollo Client configuration

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, ApolloLink } from '@apollo/client';
 
-const httpLink = new HttpLink({ uri: 'https://travel-booking-app-2.onrender.com/api' || 'http://localhost:4000/api' ||'https://travel-booking-app-backend1.onrender.com'});
+const httpLink = new HttpLink({ uri: 'https://travel-booking-app-2.onrender.com/api' || 'http://localhost:4000/api' ||'https://travel-booking-app-backend1.onrender.com/api'});
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('auth-token');
