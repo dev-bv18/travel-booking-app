66f0542152bc3a9f2dd33337386bff89c2272d99
Baibhab <baibhabnayak.31@gmail.com>
2024-12-22 11:01:11 +0530

    new

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, ApolloLink } from '@apollo/client';
 
-const httpLink = new HttpLink({ uri: 'https://travel-booking-app-2.onrender.com/api' || 'http://localhost:4000/api' });
+const httpLink = new HttpLink({ uri: 'https://travel-booking-app-2.onrender.com/api' || 'http://localhost:4000/api' ||'https://travel-booking-app-backend1.onrender.com'});
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('auth-token');
